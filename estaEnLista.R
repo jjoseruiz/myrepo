319fddb30a404bd3c1d81ce6f10fcf3bea8ef2fc
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-04-24 21:01:17 +0200

    Cambio de bucle para comprobación si una coordenada está presente en una matriz

--- estaEnLista.R
+++ estaEnLista.R
@@ -1,16 +1,19 @@
 estaEnLista<-function(matriz,coordenada)
 {
-    for(i in 1:nrow(matriz)){
-      vacio = is.na(matriz[i,1:ncol(matriz)])
-      condicionvacio = vacio[1]&vacio[2]&vacio[3]
-      if(condicionvacio){
-        return(FALSE)
-      }else{
-        fila = matriz[i,1:ncol(matriz)]
-        if(fila[1]==coordenada[1] & fila[2]==coordenada[2] &fila[3]==coordenada[3]){
-          return(TRUE)
-        }
+  esta = FALSE
+  i=1
+  while ( i<= nrow(matriz) & !esta){
+    vacio = is.na(matriz[i,1:ncol(matriz)])
+    condicionvacio = vacio[1]&vacio[2]&vacio[3]
+    if(condicionvacio){
+      esta = FALSE
+    }else{
+      fila = matriz[i,1:ncol(matriz)]
+      if(fila[1]==coordenada[1] & fila[2]==coordenada[2] & fila[3]==coordenada[3]){
+        esta = TRUE
       }
     }
-    return(FALSE)
+    i = i+1
+  }
+  return (esta)
 }
