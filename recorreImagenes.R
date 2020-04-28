7e728fe3006abfbff745c10b3fbc95a49fc20e31
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-04-28 17:21:38 +0200

    Cambiamos una lista de coordenadas por una única matriz de coorden

--- recorreImagenes.R
+++ recorreImagenes.R
@@ -1,6 +1,14 @@
 recorreImagenes<-function(IMAGENES,COORDENADAS)
 {
-  #las IMAGENES VENDRAN EN UNA LISTA IGUAL QUE LAS COORDENADAS
-  listaValores<-list(valoresImagen(IMAGENES[i],COORDENADAS[i]))
-  return(listaValores)
+  
+  #las IMAGENES VENDRAN EN UNA LISTA 
+  #Las coordenadas serán una matriz de coordenadas
+  longitud = length(IMAGENES)
+  i = 1
+  listaValoresi = list()
+  while(i<=longitud){
+    listaValoresi<-list(listaValoresi,valoresImagen(IMAGENES[[i]],COORDENADAS))
+    i = i + 1
+  }
+  return(listaValoresi)
 }
