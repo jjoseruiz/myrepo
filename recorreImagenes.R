9951de4c33fb230fcacace54e1f1b9aab5462d5c
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-01 20:37:47 +0200

    devuelve las ventanas de una lista de imágenes

--- recorreImagenes.R
+++ recorreImagenes.R
@@ -3,12 +3,6 @@ recorreImagenes<-function(IMAGENES,COORDENADAS)
   
   #las IMAGENES VENDRAN EN UNA LISTA 
   #Las coordenadas serán una matriz de coordenadas
-  longitud = length(IMAGENES)
-  i = 1
-  listaValoresi = list()
-  while(i<=longitud){
-    listaValoresi<-list(listaValoresi,valoresImagen(IMAGENES[[i]],COORDENADAS))
-    i = i + 1
-  }
-  return(listaValoresi)
+  listaValores = lapply(IMAGENES,valoresImagen,COORDENADAS = COORDENADAS)
+  return(listaValores)
 }
