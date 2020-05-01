8b470bf57ae9a12980752c4581ef4b46f473508c
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-01 23:44:24 +0200

    corrección errores

--- aplicaFuncion.R
+++ aplicaFuncion.R
@@ -9,9 +9,22 @@ aplicaFunciones<-function(listaValores,listaFunciones)
       for(l in 1:length(features)){
         feat = c(feat,features[[l]])
       }
-      listaFeatures[1:nrow(listaFeatures),j] = feat
+      if(i == 1)
+      listaFeatures[1:(nrow(listaFeatures)/2),j] = feat
+      
+      else{
+        listaFeatures[((nrow(listaFeatures))/2+1):nrow(listaFeatures),j]=feat
+      }
+    }
+    if(i == 1){
+      #los primeros nvoxel/2 son LESION
+      listaFeatures[1:(length(listaValores[[1]])/2),ncol(listaFeatures)] = 1
+      #los segundos nvoxel/2 son SANOS
+      listaFeatures[(length(listaValores[[1]])/2+1):length(listaValores[[1]])/2,ncol(listaFeatures)] = 0
+    }else{
+      listaFeatures[((nrow(listaFeatures))/2+1):(nrow(listaFeatures)*3/4),ncol(listaFeatures)] = 1
+      listaFeatures[((nrow(listaFeatures)*3/4)+1):nrow(listaFeatures),ncol(listaFeatures)] = 0
     }
   }
-  
   return (listaFeatures)
 }
