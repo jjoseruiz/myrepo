8e3e2a8079a60f5c06f340fe84baa37488f7957c
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-01 19:19:19 +0200

    devuelve ventana de valores dada una imagen y matriz de coordenadas

--- valoresImagen.R
+++ valoresImagen.R
@@ -1,7 +1,14 @@
 valoresImagen<-function(IMAGEN,COORDENADAS){
-  valores = c()
+  coord = list()
+  if(!is.antsImage(IMAGEN)){
+    imagenants = as.antsImage(IMAGEN)
+  }else{
+    imagenants = IMAGEN
+  }
   for(i in 1:nrow(COORDENADAS)){
-    valores = c(valores,IMAGEN[COORDENADAS[i,1],COORDENADAS[i,2],COORDENADAS[i,3]])
+    ventana = getNeighborhoodAtVoxel(imagenants,center = c(COORDENADAS[i,1],COORDENADAS[i,2],COORDENADAS[i,3]),c(1,1,1))
+    valores = c(list(ventana$values),coord)
+    coord = valores
   }
   return(valores)
 }
