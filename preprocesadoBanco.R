7a4423c7ff53b6400407fd1cb1d256c842cc6790
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-01 16:08:18 +0200

    preprocesado

--- preprocesadoBanco.R
+++ preprocesadoBanco.R
@@ -1,4 +1,3 @@
-setwd("/Users/juanjoseruizpenela/Documents/WORKSPACE/TFG")
 source("correccion.R")
 source("leeImagen.R")
 source("registro.R")
@@ -9,14 +8,13 @@ library(extrantsr)
 #Configuración del registro
 s1_FLAIR_CORRECTED=correccion(readnii("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient1_FLAIR.nii.gz"))
 S1_MASK = readnii("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient1_brainmask.nii.gz")
-lT1<-c()
-lFLAIR<-c()
-lBMASK<-c()
-lB_FLAIR<-c()
-lB_T1<-c()
+
 #PARA MI DATASET DE MRI APLICAREMOS LA SIGUIENTE SECUENCIA DE PASOS
-for (i in 1:30){
+for (i in 2:30){
   ###LECTURA
+  if(i == 1){
+    IMG_MASK = S1_MASK
+  }
   print(paste0("Leyendo Imagen T1 del sujeto ",i))
   IMG_T1 = leeImagen(ROOT = "/Users/juanjoseruizpenela/Documents/IMG1/raw_images/","T1",i)
   print(paste0("Leyendo Imagen FLAIR del sujeto ",i))
@@ -43,20 +41,20 @@ for (i in 1:30){
   print("Registrando FLAIR al espacio de la FLAIR del paciente 1")
   if(i==1){
     FLAIR_CORRECT_REGISTERED = FLAIR_CORRECT
+    #registro flair
+    mascara = IMG_MASK
   }else{
     FLAIR_CORRECT_REGISTERED = registro(FLAIR_CORRECT)$outfile
+    #registro mascara
+    print("Registrando la Máscara al espacio de la Máscara del paciente 1")
+    NEW_MASK = registroMascara(IMG_MASK)$outfile
+    mascara  = array(as.integer(c(NEW_MASK)),dim = dim(NEW_MASK))
   }
- 
   #Registramos la T1
   print("Registrando T1 al espacio de la FLAIR del paciente 1")
   T1_CORRECT_REGISTERED = registro(T1_CORRECT)$outfile
   #registramos la máscara
-  if(i==1){
-    mascara = IMG_MASK
-  }else{
-    NEW_MASK = registroMascara(IMG_MASK)$outfile
-    mascara  = array(as.integer(c(NEW_MASK)),dim = dim(NEW_MASK))
-  }
+
   ##Extracción
   print("Extrayendo Cerebro de FLAIR")
   Si_FLAIR_BRAIN_REGISTERED=mask_img(FLAIR_CORRECT_REGISTERED,mascara)
@@ -67,18 +65,13 @@ for (i in 1:30){
   ##
   ws_flair =zscore_img(Si_FLAIR_BRAIN_REGISTERED)
   ##
-  ws_t1 = zscore(Si_T1_BRAIN_REGISTERED)
+  ws_t1 = zscore_img(Si_T1_BRAIN_REGISTERED)
   
   
   ###ESCRITURA
-  setwd("/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_IMAGES")
   print("Escribiendo Imágenes en la carpeta BRAIN_IMAGES")
-  write_nifti(ws_flair,paste0("S",i,"_FLAIR_BRAIN"))
-  write_nifti(ws_t1,paste0("S",i,"_T1_BRAIN"))
-  BRAIN_FLAIR_path=file.path("/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_IMAGES/",paste0("S",i,"_FLAIR_BRAIN.nii.gz"))
-  BRAIN_T1_path=file.path("/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_IMAGES/",paste0("S",i,"_T1_BRAIN.nii.gz"))
-  lB_FLAIR=c(lB_FLAIR,BRAIN_FLAIR_path)
-  lB_T1=c(lB_T1,BRAIN_T1_path)
+  write_nifti(ws_flair,paste0("/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_IMAGES/","S",i,"_FLAIR_BRAIN"))
+  write_nifti(ws_t1,paste0("/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_IMAGES/","S",i,"_T1_BRAIN"))
 }
 
 
