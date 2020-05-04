479db5377f849118547758bda04784170e5b66a7
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-04 06:18:04 +0200

    aplico funcion jésima a los vectores correspondientes a cada imágen de la fila iésima/vóxel iésimo. Escribo por columnas el resultado de la función.

--- aplicaFuncion.R
+++ aplicaFuncion.R
@@ -1,30 +1,39 @@
-aplicaFunciones<-function(listaValores,listaFunciones)
+aplicaFuncion<-function(vecinos,listaFunciones)
 {
-  listaFeatures = matrix(nrow = 2*length(listaValores[[1]]),ncol = length(listaFunciones)+1)
-  for(i in 1:length(listaValores)){
-    for (j in 1:length(listaFunciones)){
+  n = ncol(vecinos)/27
+  listaFeatures = matrix(nrow = nrow(vecinos),ncol = length(listaFunciones)*n)
+    for(i in 1:nrow(vecinos)){
+      lista = list()
+      #variables auxiliares
+      c=0
+      k=1
       #aplicamos la función j a la ventana iésimo
-      features = c(lapply(listaValores[[i]],listaFunciones[[j]]))
-      feat = c()
-      for(l in 1:length(features)){
-        feat = c(feat,features[[l]])
+      #n es el número de imágenes
+      #almacenamos en una lista. Cada posición de la lista corresponde con el conjunto de vecinos de la imagen n y vóxel iésimo
+      for(p in 1:n){
+        valuesk = list(vecinos[i,(c+1):(k*27)])
+        c=k*27
+        k=k+1
+        #almacenamos en lista los vectores de los valores cada imagen correspondientes al vóxel iésimo
+        lista = c(lista,valuesk)
       }
-      if(i == 1)
-      listaFeatures[1:(nrow(listaFeatures)/2),j] = feat
-      
-      else{
-        listaFeatures[((nrow(listaFeatures))/2+1):nrow(listaFeatures),j]=feat
+      #aplicamos la función jésima a cada elemento de la lista
+      for (j in 1:length(listaFunciones)){
+        features = c(lapply(lista,listaFunciones[[j]]))
+        #lo convertimos en vector
+        feat = c()
+        for(l in 1:length(features)){
+          feat = c(feat,features[[l]])
+        }
+        #repartimos el resultado de la función en las columnas de la matriz 
+        #razon reparto.
+        r = ncol(listaFeatures)/n
+        b=0
+        while(b<(length(listaFunciones)-2)){
+          listaFeatures[i,j+b*r]=feat[b+1]
+          b=b+1
+        }
       }
     }
-    if(i == 1){
-      #los primeros nvoxel/2 son LESION
-      listaFeatures[1:(length(listaValores[[1]])/2),ncol(listaFeatures)] = 1
-      #los segundos nvoxel/2 son SANOS
-      listaFeatures[(length(listaValores[[1]])/2+1):length(listaValores[[1]])/2,ncol(listaFeatures)] = 0
-    }else{
-      listaFeatures[((nrow(listaFeatures))/2+1):(nrow(listaFeatures)*3/4),ncol(listaFeatures)] = 1
-      listaFeatures[((nrow(listaFeatures)*3/4)+1):nrow(listaFeatures),ncol(listaFeatures)] = 0
-    }
-  }
   return (listaFeatures)
 }
