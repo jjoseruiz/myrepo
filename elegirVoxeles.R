1c131a1e4732a4cfc1b604634d1030330b6a5f90
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-04 02:11:20 +0200

    evitamos que se cojan vóxeles en los extremos

--- elegirVoxeles.R
+++ elegirVoxeles.R
@@ -18,36 +18,46 @@ elegirVoxeles<-function(num_voxel,mascara_gt)
     indices = matrix(nrow = num_voxel,ncol = 3) 
     while(i<num_voxel/2){
       coordLes = obtenCoord(sample(lesion,1),mascara_gt)
-      if(!estaEnLista(indices,coordLes) & i<num_voxel/2){
-        vecinosLes = getNeighborhoodAtVoxel(ants_mascara,coordLes,c(1,1,1))
-        values_Les = vecinosLes$values>0
-        l=0
-        while(l<length(vecinosLes$values) & i<num_voxel/2){
-          l = l+1
-          if(values_Les[l] & i<num_voxel/2 & !estaEnLista(indices,vecinosLes$indices[l,1:ncol(indices)])){
-            i = i + 1
-            print(paste0("voxel LESION Nº-->",i))
-            indices[i+j,1:ncol(indices)] = vecinosLes$indices[l,1:ncol(indices)]
+      condExtremo1=!coordLes[1]==192&!coordLes[2]==512&!coordLes[3]==512
+      condExtremo2=!coordLes[1]==1&!coordLes[2]==1&!coordLes[3]==1
+      if(condExtremo1 & condExtremo2){
+        if(!estaEnLista(indices,coordLes) & i<num_voxel/2){
+          vecinosLes = getNeighborhoodAtVoxel(ants_mascara,coordLes,c(1,1,1))
+          values_Les = vecinosLes$values>0
+          l=0
+          while(l<length(vecinosLes$values) & i<num_voxel/2){
+            l = l+1
+            if(values_Les[l] & i<num_voxel/2 & !estaEnLista(indices,vecinosLes$indices[l,1:ncol(indices)])){
+              i = i + 1
+              print(paste0("voxel LESION Nº-->",i))
+              indices[i+j,1:ncol(indices)] = vecinosLes$indices[l,1:ncol(indices)]
+            }
           }
         }
       }
     }
+
     while(j<num_voxel/2){
       coordSan = obtenCoord(sample(sano,1),mascara_gt)
-      vecinosSan = getNeighborhoodAtVoxel(ants_mascara,coordSan,c(1,1,1))
-      values_San = vecinosSan$values<1
-      l=0
-      while(l<length(vecinosSan$values) & j<num_voxel/2)
-      {
-        l = l+1
-        condSano = values_San[l]
-        if(is.na(condSano))
-          condSano = FALSE
-        if(condSano & j<num_voxel/2 & !estaEnLista(indices,vecinosSan$indices[l,1:ncol(indices)]))
+      condExtremo1=!coordSan[1]==192&!coordSan[2]==512&!coordSan[3]==512
+      condExtremo2=(!coordSan[1]==1&!coordSan[2]==1&!coordSan[3]==1)
+      if(condExtremo1&condExtremo2){
+        vecinosSan = getNeighborhoodAtVoxel(ants_mascara,coordSan,c(1,1,1))
+        values_San = vecinosSan$values<1
+        #condcion extremo
+        l=0
+        while(l<length(vecinosSan$values) & j<num_voxel/2)
         {
-          j = j + 1
-          print(paste0("voxel SANO Nº-->",j))
-          indices[i+j,1:ncol(indices)] = vecinosSan$indices[l,1:ncol(indices)]
+          l = l+1
+          condSano = values_San[l]
+          if(is.na(condSano))
+            condSano = FALSE
+          if(condSano & j<num_voxel/2 & !estaEnLista(indices,vecinosSan$indices[l,1:ncol(indices)]))
+          {
+            j = j + 1
+            print(paste0("voxel SANO Nº-->",j))
+            indices[i+j,1:ncol(indices)] = vecinosSan$indices[l,1:ncol(indices)]
+          }
         }
       }
     }
