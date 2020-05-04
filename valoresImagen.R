7043444533b57ca47718ebd5c55e10be49d3ab6a
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-04 07:55:20 +0200

    actualizado

--- valoresImagen.R
+++ valoresImagen.R
@@ -1,5 +1,6 @@
 valoresImagen<-function(IMAGEN,COORDENADAS){
-  coord = list()
+  #27 vecinos por 3 coordenadas cada uno.
+  coord = matrix(nrow = nrow(COORDENADAS),ncol = 27*3,byrow = TRUE)
   if(!is.antsImage(IMAGEN)){
     imagenants = as.antsImage(IMAGEN)
   }else{
@@ -7,10 +8,15 @@ valoresImagen<-function(IMAGEN,COORDENADAS){
   }
   for(i in 1:nrow(COORDENADAS)){
     ventana = getNeighborhoodAtVoxel(imagenants,center = c(COORDENADAS[i,1],COORDENADAS[i,2],COORDENADAS[i,3]),c(1,1,1))
-    valores = c(coord,list(ventana$values))
-    coord = valores
+    coordi = ventana$indices
+    ordenada = c()
+    for(j in 1:nrow(coordi)){
+      ordenada=c(coordi[j,1:3],ordenada)
+    }
+    #cada tres columnas tenemos las coordenadas de un vecino
+    coord[i,1:81]=ordenada
   }
   #hay que tener en cuenta que los primeros numvoxel/2 son LESION
   #Los numVoxel/2 siguientes son SANOS
-  return(valores)
+  return(coord)
 }
