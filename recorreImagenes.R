371037c19a8bcc7c4842f921fb9dae465154f869
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-04 02:15:23 +0200

    Dada una lista de imágenes y una matriz de coordenadas, devuelve una matriz de tantas filas como coordenadas y tantas columnas como imagenes*numerodevecinos

--- recorreImagenes.R
+++ recorreImagenes.R
@@ -3,6 +3,14 @@ recorreImagenes<-function(IMAGENES,COORDENADAS)
   
   #las IMAGENES VENDRAN EN UNA LISTA 
   #Las coordenadas serán una matriz de coordenadas
-  listaValores = lapply(IMAGENES,valoresImagen,COORDENADAS = COORDENADAS)
-  return(listaValores)
+  mat = matrix(nrow = nrow(COORDENADAS),ncol = length(IMAGENES)*27)
+  l=0
+  for(i in 1:length(IMAGENES)){
+    v = valoresImagen(IMAGENES[[i]],COORDENADAS)
+    valores = sacoValorVecinos(IMAGENES[[i]],v)
+    mat[1:nrow(valores),(1+l):(l+ncol(valores))]=valores
+    l=l+ncol(valores)
+  }
+  #listaValores = lapply(IMAGENES,valoresImagen,COORDENADAS = COORDENADAS)
+  return(mat)
 }
