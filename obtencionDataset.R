b2f48d718e63de6cb92bab4adce3b4f8180d8557
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-14 12:57:01 +0200

    actualizado

--- obtencionDataset.R
+++ obtencionDataset.R
@@ -8,30 +8,12 @@ source("aplicaFuncion.R")
 source("sacoValorVecinos.R")
 listaFunciones = c(mean,min,max,sd)
 numero_voxeles = 2000
-#nSujetos = devuelveImagenes()
-nSujetos=list(S1_FLAIR_BRAIN,readnii("/Users/juanjoseruizpenela/Documents/IMG1/BRAIN_IMAGES/S1_T1_BRAIN.nii.gz"))
-rootflair1 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",1,"_FLAIR_BRAIN.nii.gz")
-rootflair2 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",2,"_FLAIR_BRAIN.nii.gz")
-rootflair4 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",4,"_FLAIR_BRAIN.nii.gz")
-roott11 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",1,"_T1_BRAIN.nii.gz")
-roott12 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",2,"_T1_BRAIN.nii.gz")
-roott14 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",4,"_T1_BRAIN.nii.gz")
-lroot = list(rootflair1,rootflair2,rootflair4,roott11,roott12,roott14)
-nSujetos = lapply(lroot,antsImageRead)
-#nSujetos = list(t1s1,t2s2,flairs1,flairs2)
-#consensos = lapply(consensopath,readnii)
-rootconsenso1 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/CONSENSO/","S",1,"_CONSENSO.nii.gz")
-rootconsenso2 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/CONSENSO/","S",2,"_CONSENSO.nii.gz")
-rootconsenso4 = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/CONSENSO/","S",4,"_CONSENSO.nii.gz")
-lconsenso = list(rootconsenso1,rootconsenso2,rootconsenso4)
-consensos = lapply(lconsenso,antsImageRead)
-#consenso = readnii("/Users/juanjoseruizpenela/Documents/IMG1/raw_images/patient1_consensus_gt.nii.gz")
-#consenso = consenso1
-#consenso  = consenso2
 nSujetos = 30
 num_images_sujeto = 2
 numImagenes = 30*2
 almacen = matrix(nrow = numero_voxeles*nSujetos,ncol = length(listaFunciones)*num_images_sujeto+1)
+nombrecolumnas = c("MEAN_FLAIR","MIN_FLAIR","MAX_FLAIR","SD_FLAIR","MEAN_T1","MIN_T1","MAX_T1","SD_T1","LESION")
+colnames(almacen)<-nombrecolumnas
 j=0
 for(l in 1:nSujetos){
   rootflairl = paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/","S",l,"_FLAIR_BRAIN.nii.gz")
