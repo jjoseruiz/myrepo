02d3b1de652ea4f6ede379dc91f23d1c1d094ac9
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-21 20:07:40 +0200

    pruebas

--- obtencionDataset.R
+++ obtencionDataset.R
@@ -1,3 +1,6 @@
+library(neurobase)
+library(ANTsRCore)
+library(ANTsR)
 source("estaEnLista.R")
 source("elegirVoxeles.R")
 source("obtenCoord.R")
@@ -6,13 +9,13 @@ source("recorreImagenes.R")
 source("devuelveImagenes.R")
 source("aplicaFuncion.R")
 source("sacoValorVecinos.R")
-listaFunciones = c(mean,min,max,sd)
-numero_voxeles = 2000
+listaFunciones = c(mean,min,max,sd,median)
+numero_voxeles = 1000
 nSujetos = 30
 num_images_sujeto = 2
-numImagenes = 30*2
+numImagenes = nSujetos*num_images_sujeto
 almacen = matrix(nrow = numero_voxeles*nSujetos,ncol = length(listaFunciones)*num_images_sujeto+1)
-nombrecolumnas = c("MEAN_FLAIR","MIN_FLAIR","MAX_FLAIR","SD_FLAIR","MEAN_T1","MIN_T1","MAX_T1","SD_T1","LESION")
+nombrecolumnas = c("MEAN_FLAIR","MIN_FLAIR","MAX_FLAIR","SD_FLAIR","MEDIAN_FLAIR","MEAN_T1","MIN_T1","MAX_T1","SD_T1","MEDIAN_T1","LESION")
 colnames(almacen)<-nombrecolumnas
 j=0
 for(l in 1:nSujetos){
@@ -29,13 +32,11 @@ for(l in 1:nSujetos){
   #i será el número de imágenes y j el número de vóxeles por imagen
   dataset = aplicaFuncion(vecinos,listaFunciones)
   #sabemos que los nvoxel/2 eran sanos por lo que, la feature gt será
-
+  
   almacen[(j+1):(j+nrow(dataset)),1:ncol(dataset)]=dataset
   almacen[(j+1):(j+nrow(dataset)/2),ncol(almacen)]=1
   almacen[((j+nrow(dataset)/2)+1):(j+nrow(dataset)),ncol(almacen)]=0
   j=j+nrow(dataset)
+  print(paste0("dataset sujeto "),l)
 }
-
-
-
-
+write.csv(almacen,"almacen")
