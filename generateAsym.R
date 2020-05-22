d2a1ec6ff851770973309eb5465344a7b5a7034c
Juan Jose Ruiz Penela <juanjoseruizpenela@Juans-iMac.local>
2020-05-22 05:20:29 +0200

    generador de imagenes simétricas y asimetricas

--- generateAsym.R
+++ generateAsym.R
@@ -3,16 +3,28 @@ library(ANTsR)
 library(ANTsRCore)
 library(extrantsr)
 #importante ese orden para representar antsImagenes con ortho2 y doubleortho
-pathImagenes="/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/"
-imgFLAIR=antsImageRead(paste0(pathImagenes,"S1_FLAIR_BRAIN.nii.gz"))
-imgT1=antsImageRead(paste0(pathImagenes,"S1_T1_BRAIN.nii.gz"))
-#Eliminamos los valores negativos para no tener problemas con el registro.
-FLAIR=imgFLAIR+abs(min(imgFLAIR))
-T1=imgT1+abs(min(imgT1))
-#reflejo flair
-reflejoFlair = reflectImage(FLAIR,2,tx="AffineFast",verbose=TRUE)
-asymFlair=FLAIR-reflejoFlair$warpedmovout
-#reflejo T1
-reflejoT1=antsApplyTransforms(T1,T1,transformlist = reflejoFlair$fwdtransforms)
-asymT1=T1-reflejoT1
+for(i in 1:30)
+{
+  pathImagenes="/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/"
+  imgFLAIR=antsImageRead(paste0(pathImagenes,"S",i,"_FLAIR_BRAIN.nii.gz"))
+  imgT1=antsImageRead(paste0(pathImagenes,"S",i,"_T1_BRAIN.nii.gz"))
+  #Eliminamos los valores negativos para no tener problemas con el registro.
+  mf=min(imgFLAIR)
+  mt=min(imgT1)
+  FLAIR=imgFLAIR-mf+1
+  T1=imgT1-mt+1
+  #reflejo flair
+  reflejoFlair = reflectImage(FLAIR,0,"AffineFast",verbose=TRUE)
+  reflejoFlair=reflejoFlair$warpedmovout+mf-1
+  asymFlair=FLAIR-reflejoFlair
+  antsImageWrite(reflejoFlair,paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S",i,"_FLAIR_SIMETRICA.nii.gz"))
+  antsImageWrite(asymFlair,paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S",i,"_FLAIR_ASIMETRICA.nii.gz"))
+  #reflejo T1
+  reflejoT1=reflectImage(T1,0,"AffineFast",verbose=TRUE)
+  reflejoT1=reflejoT1$warpedmovout+mt-1
+  asymT1=T1-reflejoT1
+  antsImageWrite(reflejoT1,paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S",i,"_T1_SIMETRICA.nii.gz"))
+  antsImageWrite(asymT1,paste0("/Users/juanjoseruizpenela/Documents/GIT REPOSITORY/myrepo/BRAIN_IMAGES/SIMETRIA/S",i,"_T1_ASIMETRICA.nii.gz"))
+}
+
 
